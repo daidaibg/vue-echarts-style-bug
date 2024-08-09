71e220728f1ec550285735299b76aac1cf9202b3
daidai <daidaibg@163.com>
2024-08-09 14:38:24 +0800

    init: vue-echarts样式i加载bug

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,8 +3,7 @@
 // 参考：
 declare module "vue" {
   export interface GlobalComponents {
-    YhButton: typeof import("../src/components/button/src/button.vue")["default"];
-    YhContent: typeof import("../src/components/layout/yh-content/yh-content.vue")["default"];
+  
   }
 }
 
